47bcf8fa9a72f9878630394351753749af3677c9
aalissonaq <aalissonalmeidaq@gmail.com>
2023-04-27 13:09:17 -0300

    ts

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -5,7 +5,7 @@ export default function Card() {
 
   return (
     <>
-      <div className="m-1 overflow-hidden shadow-lg max-w-min rounded-3xl">
+      <div className="m-2 overflow-hidden shadow-lg max-w-min rounded-3xl">
         <div className="justify-center px-3 py-4 ">
           <div className="flex flex-row ">
             <div className="px-5 pt-1 mb-2 text-3xl font-bold text-green-600 align-middle ">
