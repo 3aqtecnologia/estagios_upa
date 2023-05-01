cb0175faae68697fadc54181b3e812eb7b1c3428
aalissonaq <aalissonalmeidaq@gmail.com>
2023-05-01 00:29:19 -0300

    Ajustes tamanho da tela de listagem de estagios

--- src/app/estagios/page.tsx
+++ src/app/estagios/page.tsx
@@ -4,14 +4,14 @@ import Card from "./components/Card";
 
 export default function Estagios() {
   return (
-    <main className="container mx-auto mt-2 min-w-xl">
-      <div className="h-48 px-10 mb-5 text-6xl font-bold text-white align-middle bg-yellow-500 rounded-xl py-14">
+    <main className="container h-screen mx-auto border border-transparent border-gray-50 min-w-xl">
+      <div className="h-48 px-10 mt-3 mb-5 text-6xl font-bold text-white align-middle bg-yellow-500 rounded-xl py-14">
         <div className="flex flex-row items-center px-5 align-middle">
           <span className="mr-5 "><RiSearchEyeLine className="text-8xl" /></span>
           <span className="text-6xl">Vagas de Estágio</span>
         </div>
       </div>
-      <div className="flex flex-row min-h-screen">
+      <div className="flex flex-row ">
         <div className="mr-6 ">
           <div className="h-64 px-10 pt-8 mr-4 bg-white border border-gray-400 rounded-xl w-60">
             <p className="text-lg font-light text-center">Filtre sua pesquisa</p>
@@ -56,6 +56,7 @@ export default function Estagios() {
             <Card />
             <Card />
             <Card />
+
           </div>
 
         </div>
