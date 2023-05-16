0ac7948d4824c7880b0c9c96d89c947100252188
aalissonaq <aalissonalmeidaq@gmail.com>
2023-05-16 12:21:36 -0300

    test

--- src/app/estagios/[detalhes]/page.tsx
+++ src/app/estagios/[detalhes]/page.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from "react";
-import { getVagaByTitle } from "./../services/get-vaga-by-title";
+import { getVagas } from "../services/get-vagas";
+import { } from "./../services/get-vaga-by-title";
 
 type VagasProps = {
+  filter: any;
   id: string;
   codigo: number;
   titulo: string;
@@ -13,11 +15,19 @@ type VagasProps = {
 export default async function UserDetailsPage({ params }: { params: { titulo: string } }) {
 
 
-  const user: VagasProps = await getVagaByTitle(params.titulo);
+  const user = await getVagas();
+  // console.log(user)
+
+  const nv = { ...user }
+  console.log(nv[3])
+  // const vfilte = user.filter(title == params.titulo);
+  // const vf = user.filter((vaga: any) => user.titulo === 'ESTÁGIO');
+  // console.log(vf);
+
 
   return (
     <>
-      <h2>teste: {user.titulo}</h2>
+
     </>
   );
 }
