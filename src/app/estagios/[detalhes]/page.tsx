1e2ada3912b366007310a9bbacd12c42cb3acdd2
aalissonaq <aalissonalmeidaq@gmail.com>
2023-06-06 11:53:27 -0300

    novo teste

--- src/app/estagios/[detalhes]/page.tsx
+++ src/app/estagios/[detalhes]/page.tsx
@@ -1,27 +1,28 @@
-import { ReactNode } from "react";
+'use client'
 
-
-type VagasProps = {
-  filter: any;
-  id: string;
-  codigo: number;
-  titulo: string;
-  tipoVaga: string;
-  cursos: ReactNode;
-  cidade: ReactNode;
-  concedente: string;
-}
-export default async function UserDetailsPage({ params, }: { params: { titulo: string } }) {
+// type VagasProps = {
+//   filter: any;
+//   id: string;
+//   codigo: number;
+//   titulo: string;
+//   tipoVaga: string;
+//   cursos: ReactNode;
+//   cidade: ReactNode;
+//   concedente: string;
+// }
+export default async function userDetailsPage({ params }: {
+  params: { id: string }
+}) {
 
 
   // const vaga: VagasProps = await getVagaByTitle('itaipu');
-  const response = await fetch(`https://estagio.universidadepatativa.com.br/api/v1/vagas?busca=${params}`);
+  const response = await fetch(`https://estagio.universidadepatativa.com.br/api/v1/vagas?busca=estagio`);
   const vagad = await response.json();
   console.log(vagad)
   console.log(params)
 
-  const nv = { ...vagad }
-  console.log(nv)
+  // const nv = { ...vagad }
+  // console.log(nv)
   // const vfilte = user.filter(title == params.titulo);
   // const vf = user.filter((vaga: any) => user.titulo === 'ESTÁGIO');
   // console.log(vf);
@@ -29,7 +30,7 @@ export default async function UserDetailsPage({ params, }: { params: { titulo: s
 
   return (
     <>
-      {params.titulo}
+      <h1>Parametro: {params.id}</h1>
     </>
   );
 }
